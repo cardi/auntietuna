0b6e474eebf744cb4b94e15b0df04120c798237b
Calvin Ardi <calvin@isi.edu>
2019-11-27 20:42:35 -0800

    popup.js: guard console logging behind DEBUG

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,11 +5,12 @@
 (async function(){
 
 // global variables
-let storage = browser.storage.local;
+const storage = browser.storage.local;
+let DEBUG     = false;
 
-// function defs
+// function defs ///////////////////////////////////////////////////////
 function onError(error) {
-  console.error("[popup]", `${error}`);
+  DEBUG && console.error("[popup]", `${error}`);
 }
 
 async function getCurrentTab() {
@@ -20,9 +21,7 @@ async function getCurrentTab() {
 
   let tabs = await browser.tabs.query(queryInfo);
   let tab = tabs[0];
-  console.assert(typeof tab.url == 'string', 'tab.url should be a string');
-
-  //console.debug("tab", tab);
+  DEBUG && console.assert(typeof tab.url == 'string', 'tab.url should be a string');
 
   return tab;
 }
@@ -34,12 +33,12 @@ function sendBgMessage(message) {
   });
 
   msg.then( response => {
-    console.log("[popup/sendBgMessage] response:", response);
+    DEBUG && console.log("[popup/sendBgMessage] response:", response);
   });
 }
 
 async function downloadHashes(addToStorage) {
-  console.assert(typeof addToStorage == 'boolean', 'addToStorage should be a boolean');
+  DEBUG && console.assert(typeof addToStorage == 'boolean', 'addToStorage should be a boolean');
 
   if (addToStorage == false) {
     renderStatus("downloading hashes.");
@@ -54,15 +53,15 @@ async function downloadHashes(addToStorage) {
   }
 
   sendBgMessage("from: " + tab.id + " url: " + tab.url);
-  console.log('[popup/download] current tab id is', tab.id);
+  DEBUG && console.log('[popup/download] current tab id is', tab.id);
 
   // DEBUG: sent a test message to tab:contentscript
-  let testMsg = browser.tabs.sendMessage(tab.id,
+  /*let testMsg = browser.tabs.sendMessage(tab.id,
     { action: 'log', msg: 'hello from ' + document.location.href }
   );
   testMsg.then( (resp) => {
     console.log("[popup/testmsg] sent hello, got back:", resp.response);
-  })
+  })//*/
 
   // send a request for the page's hashes to tab:content script
   let domRequestMsg = browser.tabs.sendMessage(tab.id,
@@ -73,7 +72,7 @@ async function downloadHashes(addToStorage) {
       last_updated : (new Date(resp.last_updated)).toJSON(),
       hashes       : resp.hashes
     }
-    console.log("[popup/domRequest] returned data:", data);
+    DEBUG && console.log("[popup/domRequest] returned data:", data);
 
     // build json blob for saving
     let blob = new Blob([JSON.stringify(data)], {type: "application/json;charset=utf-8"});
@@ -83,7 +82,7 @@ async function downloadHashes(addToStorage) {
     let url = window.URL.createObjectURL(blob);
     let name = `hashes-${date.toJSON()}.json`;
 
-    console.log("[popup/exportHashes]", "filename:", name, "url:", url);
+    DEBUG && console.log("[popup/exportHashes]", "filename:", name, "url:", url);
 
     if (addToStorage == false) {
       // download blob: use FileSaver to save file
@@ -97,7 +96,7 @@ async function downloadHashes(addToStorage) {
 
       // process response
       msgSendImport.then( msgResp => {
-        console.log("[popup/msgSendImport] response from background:", msgResp.response);
+        DEBUG && console.log("[popup/msgSendImport] response from background:", msgResp.response);
         renderStatus("page added.");
       });
     }
@@ -116,11 +115,13 @@ function renderStatus(statusText) {
 ////////////////////////////////////////////////////////////////////////
 // entry point /////////////////////////////////////////////////////////
 
-// enable debug button
-storage.get("debug").then( (result) => {
+await storage.get("debug").then( (result) => {
   if("debug" in result) {
     if(result.debug === true) {
+      // enable debug button
       document.getElementById('download').style.display = "block";
+      // enable debugging messages
+      DEBUG = true;
     }
   }
 }, onError);
